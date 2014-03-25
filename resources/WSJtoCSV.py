7755093e98254a3c1bec8ef24c0fdb00a7d36f93
CharlesBean <beanchar@msu.edu>
2014-03-25 15:33:09 -0400

    Wrote (again..) PyScript for converting WSJ files to CSV.

---
+++ resources/WSJtoCSV.py
@@ -0,0 +1,97 @@
+################# WSJtoCSV ##################
+##
+## Charles Bean
+## 3/25/14
+##
+#############################################
+
+########### Parameters ###########
+
+rootdir = "/Users/charlesbean/Code/TEvA/Corpora/Testbed"
+
+outdir = "/Users/charlesbean/Code/TEvA/Corpora/Converted/WSJ/Extracted"
+
+refdir = "/Users/charlesbean/Code/TEvA/Corpora/Converted/WSJ/Ref Files"
+
+##################################
+
+import os
+import sys
+import string
+
+__author__ = 'charlesbean'
+
+class ConvertedICMI(object):
+
+    def __init__(self):
+        self.file = ""
+
+    def __directory_check__(self, rootdir):
+        """ Algorithm:
+                1. Used for error checking
+                2. Iterates through folders/files in directory, prints out
+        """
+        print "\n==========================\nDirectory: %s\nFiles taken:\n" % rootdir
+        article_count = 0
+        for subdir, dirs, files in os.walk(rootdir):
+            for file in files:
+                if file != ".DS_Store":
+                    print " " + subdir + "/" + file
+                    article_count += 1
+        print "\nNumber of files: %s \n==========================\n" % article_count
+
+    def iterate(self):
+        for subdir, dirs, files in os.walk(rootdir):
+            for file in files:
+                if file != ".DS_Store":
+                    self.file = subdir + "/" + file
+                    self.process(outdir + "/" + file + ".csv", refdir + "/" + file + ".csv.ref")
+
+    def process(self, outFilename, refFilename):
+        id = 0
+        replyTo = -1
+        start = 5 #Seconds
+        author = "chucknorris"
+
+        refFile = open(refFilename, "w")
+
+        outFile = open(outFilename, "w")
+        outFile.write("id\treplyTo\tstart\tauthor\ttext\n")
+
+        with open(self.file, "r") as open_file:
+            for line in open_file:
+                if line != "==========\n":
+                    if (replyTo == -1):
+                        text = str(id) + '\t' + "" + '\t' + str(start) + '\t' + author + '\t' + line
+                        id += 1
+                        replyTo += 1
+                        start += 5
+
+
+                        outFile.write(text)
+                        refFile.write(str(0) + '\n')
+                    else:
+                        text = str(id) + '\t' + str(replyTo) + '\t' + str(start) + '\t' + author + '\t' + line
+                        id += 1
+                        replyTo += 1
+                        start += 5
+
+
+                        outFile.write(text)
+                        refFile.write(str(0) + '\n')
+
+                else:
+                    refFile.write(str(1) + '\n')
+
+
+
+
+def main():
+    Converted = ConvertedICMI()
+    Converted.__directory_check__(rootdir)
+    Converted.iterate()
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ src/edu/msu/mi/teva/wsj/WSJTokenizer.java
@@ -0,0 +1,7 @@
+package edu.msu.mi.teva.wsj;
+
+/**
+ * Created by charlesbean on 3/25/14.
+ */
+public class WSJTokenizer {
+}
